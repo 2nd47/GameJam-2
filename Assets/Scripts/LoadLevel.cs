0e3be4d5a769e56aa228ffb9d19e31b4ecd499b3
Leora R <lrogovein@hotmail.ca>
2017-01-21 15:08:23 -0500

    Lose/Win Screen

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadLevel : MonoBehaviour {
 
 	public void LoadScene(string sceneName)
 	{ 
-		Application.LoadLevel (sceneName);
+		//Application.LoadLevel (sceneName);
+		SceneManager.LoadScene(sceneName);
 	}
 }
