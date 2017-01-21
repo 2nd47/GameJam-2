92af92d45ceaff936b54dbd96d06d9456ccd880b
Jasmin Lantos <jnlantos@gmail.com>
2017-01-21 13:36:32 -0500

    delete bouncycollider script and reset funhouse to origin

---
+++ Assets/Scripts/ClownMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClownMovement : MonoBehaviour {
+
+
+
+	Transform player;               // Reference to the player's position.
+	UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+	void Awake ()
+	{
+		// Set up the references.
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+
+	void Update ()
+	{
+	}
+}
