2ba411570f3d5a0f450fa34fd9251a91c75c1d43
Christine Murad <christine.murad@mail.utoronto.ca>
2017-01-21 14:01:21 -0500

    Made character rotate

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,15 +14,39 @@ public class Player : MonoBehaviour {
 		if (Input.GetKey(KeyCode.UpArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
 			Vector3.Lerp(transform.position, transform.position += new Vector3(0.0f, 0.0f, 0.1f), 5.0f);
+
+			float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+			float moveVertical = Input.GetAxisRaw ("Vertical");
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+			transform.rotation = Quaternion.LookRotation(movement);
+			transform.Translate (movement *  5.0f * Time.deltaTime, Space.World);
 		} else if (Input.GetKey(KeyCode.DownArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
 			Vector3.Lerp(transform.position, transform.position += new Vector3(0.0f, 0.0f, -0.1f), 5.0f);
+
+			float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+			float moveVertical = Input.GetAxisRaw ("Vertical");
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+			transform.rotation = Quaternion.LookRotation(movement);
+			transform.Translate (movement *  5.0f * Time.deltaTime, Space.World);
 		} else if (Input.GetKey(KeyCode.LeftArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
 			Vector3.Lerp(transform.position, transform.position += new Vector3(-0.1f, 0.0f, 0.0f), 5.0f);
+
+			float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+			float moveVertical = Input.GetAxisRaw ("Vertical");
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+			transform.rotation = Quaternion.LookRotation(movement);
+			transform.Translate (movement *  5.0f * Time.deltaTime, Space.World);
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
 			Vector3.Lerp(transform.position, transform.position += new Vector3(0.1f, 0.0f, 0.0f), 5.0f);
+
+			float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+			float moveVertical = Input.GetAxisRaw ("Vertical");
+			Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+			transform.rotation = Quaternion.LookRotation(movement);
+			transform.Translate (movement *  5.0f * Time.deltaTime, Space.World);
 		}
 	}
 }
