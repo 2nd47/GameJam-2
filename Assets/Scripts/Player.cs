29c96e76478afca8663a0677bb259ddf74da826e
Christine Murad <christine.murad@mail.utoronto.ca>
2017-01-21 13:48:01 -0500

    Moved characters and changed floor colour

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,14 +13,16 @@ public class Player : MonoBehaviour {
 	void Update () {
 		if (Input.GetKey(KeyCode.UpArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
+			Vector3.Lerp(transform.position, transform.position += new Vector3(0.0f, 0.0f, 0.1f), 5.0f);
 		} else if (Input.GetKey(KeyCode.DownArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
+			Vector3.Lerp(transform.position, transform.position += new Vector3(0.0f, 0.0f, -0.1f), 5.0f);
 		} else if (Input.GetKey(KeyCode.LeftArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
-			Vector3.Lerp(transform.position, transform.position += new Vector3(-2,0,0),  1.0f);
+			Vector3.Lerp(transform.position, transform.position += new Vector3(-0.1f, 0.0f, 0.0f), 5.0f);
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
 			GetComponent<Animator>().SetTrigger("Walk");
-			transform.position += new Vector3(2,0,0);
+			Vector3.Lerp(transform.position, transform.position += new Vector3(0.1f, 0.0f, 0.0f), 5.0f);
 		}
 	}
 }
