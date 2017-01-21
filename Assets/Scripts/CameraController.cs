cd5f17008579ec4147b7b246da2c69375b0ceb23
2nd47 <2nd4777@gmail.com>
2017-01-21 15:24:26 -0500

    camera and player controls improved

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
 	public GameObject player;
+	public float damping = 1;
 
 	private Vector3 offset;
 
@@ -14,7 +15,10 @@ public class CameraController : MonoBehaviour {
 
 	void LateUpdate ()
 	{
-		transform.position = player.transform.position + offset;
+		float desiredAngle = player.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+		Vector3 desiredPosition = player.transform.position + (rotation * offset);
+		transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping * 2);
+		transform.LookAt (player.transform.position);
 	}
 }
-
